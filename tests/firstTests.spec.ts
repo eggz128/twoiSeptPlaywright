a51f059c689f4481a00aeff599b6f88cb2ef010c
Steve Powell <steve.powell@edgewords.co.uk>
2025-09-16 09:45:27 +0100

    Initial commit (after day 1)

---
+++ tests/firstTests.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+//Test method takes 2 args
+//1st arg = test name
+//2nd arg = call back function containing the test code
+test("My First Test", async ({page})=>{
+    //driver.get("https://www.edgewordstraining.co.uk/webdriver2")
+    await page.goto("https://www.edgewordstraining.co.uk/webdriver2");
+    //driver.findElement(By.cssSelector("#menu > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)")).click()
+    await page.locator("#menu > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)").click();
+    
+    //WebDriver can interact with locators that match more than one thing. Playwright cannot - it needs the locator to uniquely match just one element
+    //await page.locator("#username").fill("edgewords") //Errors with: "Strict mode violation"
+
+    //There are *two* elements with an id of "username" - the page uses invalid markup
+
+    //To fix this we can filter down the 2 matches - in this case selecting the first (0th) match
+    await page.locator("#username").nth(0).fill("edgewords") //Will also .clear()
+
+})
+
---
+++ tests/test-1.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+
+//The callback function can request a Playwright "Fixture" - { page } in this case
+test('test', async ({ page }) => { //PW will set up a browser with a context for this test, and set up a page in that ready for us to use 
+  // Recording...
+  await page.goto('https://www.edgewordstraining.co.uk/webdriver2/');
+  await page.getByRole('link', { name: 'Login To Restricted Area' }).click();
+  await page.getByRole('row', { name: 'User Name?' }).locator('#username').click();
+  await page.getByRole('row', { name: 'User Name?' }).locator('#username').fill('edgewords');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('edgewords123');
+  await page.getByRole('link', { name: 'Submit' }).click();
+  //No need for a wait - the assertion will retry if it doesnt find the element , or it has the "wrong" text
+  await expect(page.locator('h1')).toContainText('Add A Record To the Database');
+
+});
+
+test('test2', async ({ page }) => { //PW can reuse the browser if open. It will establish a new context and page for each test to use
+  
+  //No waits required. PW will auto wait for animations, scrolling and elements if required.
+
+  //...except we do need to "await" steps.
+  //all these steps send instructions to the browser that will be completed at some "future" point in time
+  //without "await" - all code potentially "executes" before any actual actions/effects have taken place
+  //The test may "complete" before the browser has had a chance to actually "do" anything
+
+  //With "await" execution is paused until the browser says the step has completed.
+
+  //In technical terms, each PW API call immediately returns a "promise" of a future action
+  //await(ing) waits for the promise to finally "resolve" before continuing
+  await page.goto('https://www.edgewordstraining.co.uk/demo-site/');
+  await page.getByRole('searchbox', { name: 'Search for:' }).click();
+  await page.getByRole('searchbox', { name: 'Search for:' }).fill('cap');
+  await page.getByRole('searchbox', { name: 'Search for:' }).press('Enter');
+  await page.getByRole('button', { name: 'Add to cart' }).click();
+  await page.locator('#content').getByRole('link', { name: 'View cart ' }).click();
+  await page.getByRole('link', { name: 'Remove this item' }).click();
+  await page.getByRole('link', { name: 'Return to shop' }).click();
+  await page.locator('#menu-item-42').getByRole('link', { name: 'Home' }).click();
+
+});
+
+
+//Be careful with "Record at cursor" - you must be in a test() methods callback function when recording
+//If you record out here you will break the file (preventing it from compiling & runnning)
+
+  // await page.getByRole('link', { name: 'Remove this item' }).click();
+  // await page.getByRole('link', { name: 'Return to shop' }).click();
