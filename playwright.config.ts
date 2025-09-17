b9fed783d8492ef363dd526852f3f168ea88e396
Steve Powell <steve.powell@edgewords.co.uk>
2025-09-17 11:21:08 +0100

    Waits

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000, //Sets max test execution time 
+  expect: {timeout: 7 * 1000}, //Default is 5s - now all expects have 7 seconds
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -32,6 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: false,
+    actionTimeout: 5000,
     //launchOptions: {slowMo: 5000 }
   },
 
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+test.setTimeout(0);
 test("Capturing values", async ({ page }) => {
     test.setTimeout(0); //Workaround PW v1.55 debugging issue
     await page.goto("https://www.edgewordstraining.co.uk/webdriver2/docs/forms.html");
@@ -26,10 +26,11 @@ test("Capturing values", async ({ page }) => {
     console.log("The text box actually contains: " + textBoxText);
 
     await page.$eval('#textInput', elm => {
-        console.log(typeof(elm)) //Writes to browser's console! Not NodeJS console.
+        console.log(typeof (elm)) //Writes to browser's console! Not NodeJS console.
     });
 
     expect(textBoxText).toBe("Hello World");
+    await expect(page).toHaveTitle("Forms", { timeout: 5000 }) //Override global config with a custom expect timeout
 });
 
 test("Generic methods", async ({ page }) => {
@@ -54,3 +55,40 @@ test("Generic methods", async ({ page }) => {
     }
 })
 
+test('Waits', async ({ page }) => {
+    test.setTimeout(15000); //Override global config test time out for just this test
+    page.setDefaultTimeout(7000); //Actions in this test get 7 seconds
+    await page.goto('http://www.edgewordstraining.co.uk/webdriver2/');
+    await page.getByRole('link', { name: 'Access Basic Examples Area' }).click();
+    await page.getByRole('link', { name: 'Dynamic Content' }).click();
+    await page.locator('#delay').click();
+    await page.locator('#delay').fill('10');
+    await page.getByRole('link', { name: 'Load Content' }).click();
+    //await page.locator('#image-holder > img').click({timeout: 5000}); //Local action timeout override
+    //Explicit wait for the apple
+    //await page.locator('#image-holder > img').waitFor({timeout: 12000});
+    await page.locator('img').waitFor({ state: 'hidden', timeout: 12000 }); //Wait for the 'spinner' to go away
+    await page.locator('#image-holder > img').click();
+    await page.getByRole('link', { name: 'Home' }).click();
+})
+
+test("Waiting for a pop up window", async ({ page, context }) => {
+    
+    await page.goto("https://www.edgewordstraining.co.uk/webdriver2/docs/dynamicContent.html")
+
+    //[a,b] = [10,20] - aray destructuring syntax will assign a=10 b=20
+    //Below we discard the second promise return value - we only need the first which gets us a handle to the new page
+    const [newSpawnedPage] = await Promise.all([ //When these two "future" actions complete return the new page fixture
+        context.waitForEvent('page'),
+        page.locator("#right-column > a[onclick='return popUpWindow();']").click()
+    ])
+
+    await page.waitForTimeout(2000); //Thread.sleep(2000);
+
+
+    const closeBtn = newSpawnedPage.getByRole('link', { name: 'Close Window' }) //Fx closes the pop up here?
+    await closeBtn.click();
+
+    await page.getByRole('link', { name: 'Load Content' }).click();
+
+})
